4bf355d4b46d40789f20602a4411895720ea0296
Norton <norton.nra27@gmail.com>
2016-11-05 22:30:27 +0700

    RecyclerView Grid

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/Adapter/hotelAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/Adapter/hotelAdapter.java
@@ -37,7 +37,6 @@ public class hotelAdapter extends RecyclerView.Adapter<hotelAdapter.ViewHolder> 
     public void onBindViewHolder(ViewHolder holder, int position) {
         hotel hotel = hotelList.get(position);
         holder.tvJudul.setText(hotel.judul);
-        holder.tvDeskripsi.setText(hotel.deskripsi);
         holder.ivFoto.setImageDrawable(hotel.foto);
     }
 
@@ -51,13 +50,11 @@ public class hotelAdapter extends RecyclerView.Adapter<hotelAdapter.ViewHolder> 
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView ivFoto;
         TextView tvJudul;
-        TextView tvDeskripsi;
 
         public ViewHolder(View itemView) {
             super(itemView);
             ivFoto = (ImageView) itemView.findViewById(R.id.imageView);
             tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
-            tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
         }
     }
 }
